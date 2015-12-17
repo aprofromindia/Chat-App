a98ca5ca0fca26ee37b79538b1bbd98eb214fcd5
Apro <aprofromuk-work@yahoo.co.in>
2015-12-17 22:35:12 +0100

    Initial Commit

---
+++ Chat App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat App
+//
+//  Created by Apro on 17/12/15.
+//  Copyright © 2015 Apro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat App
+//
+//  Created by Apro on 17/12/15.
+//  Copyright © 2015 Apro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
